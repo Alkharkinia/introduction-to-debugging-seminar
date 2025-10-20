4559b3b43a4d64099892d3402e870a2aa944f6ba
Alkharkinia <162748046+Alkharkinia@users.noreply.github.com>
2025-10-20 15:32:33 +0300

    End session question added

---
+++ SeminarOne/src/debugtrie/DebugExampleQuestion.java
@@ -0,0 +1,20 @@
+package Seminar;
+
+public class DebugExampleQuestion {
+    public static void main(String[] args) {
+        int[] numbers = {11, 20, 30, 40, 51};
+        double average = calculateAverage(numbers);
+        System.out.println("Ortalama: " + average);
+    }
+
+    public static double calculateAverage(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i <= nums.length; i++) { 
+            sum += nums[i];
+        }
+
+        double average = sum / nums.length; 
+        return average;
+    }
+}
